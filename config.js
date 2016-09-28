b0dd352e16b71d13634265b9ffdb160afd8ffae3
priyansh gupta <priyanshgupta1161@gmail.com>
2016-09-29 01:14:06 +0530

    mlabs url added

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 module.exports = {
   DEV_URL : 'localhost:3000',
   DEV_DB_URL : 'mongodb://127.0.0.1:27017/minor_v1',
-  PRODUCTION_DB_URL : ''
+  PRODUCTION_DB_URL : 'mongodb://test:test@ds046939.mlab.com:46939/minor1'
 };
