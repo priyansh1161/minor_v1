6876651e9ef5988713edc9cd3c222ee6279bd440
priyansh gupta <priyanshgupta1161@gmail.com>
2016-10-03 12:29:10 +0530

    temp testing

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var temp = require('./temp');
 
 require('./device_api/db/dbConfig');
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/device', exposed);
 app.use('/signup', signUp);
+app.use('/temp', temp);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
